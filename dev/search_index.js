8f0c0d3d0dffe1a4d0f4239ae9a397bb149e1458
Documenter.jl <documenter@juliadocs.github.io>
2021-10-07 11:19:15 +0000

    build based on 44d4069

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JGraphs","category":"page"},{"location":"#JGraphs","page":"Home","title":"JGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JGraphs]","category":"page"}]
+}
