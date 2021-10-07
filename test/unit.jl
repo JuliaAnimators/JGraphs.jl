321d840568353160583ee4e70dc99bf338ecdb08
gpucce <g.puccetti92@gmail.com>
2021-10-07 11:39:02 +0200

    adjust testing

--- test/unit.jl
+++ test/unit.jl
@@ -1,5 +1,19 @@
 
 
+@testset "tmpdir" begin
+
+    #=
+    Leftover files from failed tests or errors can cause testing errors.
+    Therefore, we remove any files in the `images` directory not pertinent to testing before executing a test.
+    =#
+
+    EXCLUDE_FILES = [".keep"]
+    for f in readdir("images")
+        !(f in EXCLUDE_FILES) && rm("images/$f")
+    end
+    @test length(readdir("images")) == length(EXCLUDE_FILES)
+end
+
 @testset "draw node" begin
 
     testvideo = Video(200, 200)
